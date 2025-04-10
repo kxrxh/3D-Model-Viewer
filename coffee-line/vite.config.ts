f2e99887469191be5eb8501587fc5c0b709aff59
kxrxh <kxrxh.contact@gmail.com>
2025-04-10 12:00:52 +0300

    feat: update Vite configuration to enhance server and preview settings
    - Added host and allowedHosts options to both server and preview configurations in vite.config.ts for improved accessibility.
    - Ensured compatibility with specified domains for local development and preview environments.

--- coffee-line/vite.config.ts
+++ coffee-line/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	preview: {
+		host: true,
+		allowedHosts: ["www.assembly3d.ru", "assembly3d.ru"],
+	},
+	server: {
+		host: true,
+		allowedHosts: ["www.assembly3d.ru", "assembly3d.ru"],
+	},
+});
